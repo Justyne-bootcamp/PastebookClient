4c72df362293f7cf8551305c05fb06c7a363f37f
Justyne-bootcamp <justyne.namay@pointwest.com.ph>
2022-05-13 09:08:15 +0800

    add routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumComponent } from './album/album.component';
+import { FriendsComponent } from './friends/friends.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'pastebook.com/{username}', component: ProfileComponent},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'album', component: AlbumComponent},
+  {path: 'timeline', component: TimelineComponent},
+  {path: 'friends', component: FriendsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
