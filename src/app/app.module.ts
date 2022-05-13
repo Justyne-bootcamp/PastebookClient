abfeaa1c8012619ed705c9513f09982465594093
Justyne-bootcamp <justyne.namay@pointwest.com.ph>
2022-05-13 08:53:14 +0800

    create components needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FriendsComponent } from './friends/friends.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProfileComponent,
+    FriendsComponent,
+    TimelineComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
