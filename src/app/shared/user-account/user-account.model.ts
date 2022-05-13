55fffff2e01c1821cd90dc0e7b25bfede7d3cdea
rkcbalanza <102932930+rkcbalanza@users.noreply.github.com>
2022-05-13 15:18:00 +0800

    push register

---
+++ src/app/shared/user-account/user-account.model.ts
@@ -0,0 +1,13 @@
+export class UserAccount {
+    userAccountId:string;
+    firstName:string;
+    lastName:string;
+    email:string;
+    password:string;
+    birthday:Date;
+    gender:string;
+    mobileNumber:string;
+    aboutMe:string;
+    profilePhotoPath:string;
+    username:string;
+}
