ae421c0a6ca786fe4d7f863f0e484976a23223c3
chesterseda <rolandchesterseda@gmail.com>
2022-05-16 16:28:34 +0800

    home and timeline UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AlbumComponent } from './album/album.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserAccountService } from './user-account.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ProfileComponent,
     FriendsComponent,
     TimelineComponent,
-    AlbumComponent
+    AlbumComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
